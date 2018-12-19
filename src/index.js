b89d3ae7a878cd59726502398a52b9c7e12d4c90
Simon VDB <simonvdbroeck@gmail.com>
2018-12-19 13:12:17 +0100

    Add instructions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-const container = window.document.getElementById('root');
+// install pixi.js
+
+// Select the DOM element with id: "root"
+
+// Create a PixiJS Application
+
+// Render out a circle graphic in the middle of the container
+
+// Make the circle move horizontally across the screen
+
+// switch direction when hitting the edge of the screen.
